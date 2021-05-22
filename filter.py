f1852d44fe15d15fe45fdf4cb11b4687790e7d11
jordan-kim <77437180+jordan-kim@users.noreply.github.com>
2021-05-22 15:00:08 +0900

    Update filter.py

--- filter.py
+++ filter.py
@@ -3,5 +3,5 @@ import glob
 all_LMZ = glob.glob('C:/Users/junsu/Desktop/**/*LMZ*.xml',
                         recursive = True)
 # C:/Users/junsu/Desktop
-#
+#C:/Users/dream/Desktop/수업자료/3-1학기/공프2/PE_project
 #
