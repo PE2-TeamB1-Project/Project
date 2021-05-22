640599abe1d4ca3e41de725d8f1c3db626944e03
junsusong98 <74344434+junsusong98@users.noreply.github.com>
2021-05-22 14:51:22 +0900

    Delete Wavelength.py
    capital

--- Wavelength.py
+++
@@ -1,6 +0,0 @@
-import xml.etree.ElementTree as ET
-
-def Wavelength(x):
-    tree = ET.parse(x)
-    d = tree.find('./ElectroOpticalMeasurements/ModulatorSite/Modulator/DeviceInfo/DesignParameters/DesignParameter[2]')
-    return d.text
